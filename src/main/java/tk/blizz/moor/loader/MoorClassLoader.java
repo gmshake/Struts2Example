fb984c9f39932a9d95eb0693e769af5c9c413f98
Zhenlei Huang <zlei.huang@gmail.com>
2013-06-01 05:47:05 +0800

    update: use URLs in MoorClassLoader

--- src/main/java/tk/blizz/moor/loader/MoorClassLoader.java
+++ src/main/java/tk/blizz/moor/loader/MoorClassLoader.java
@@ -2,7 +2,6 @@ package tk.blizz.moor.loader;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
@@ -11,13 +10,12 @@ import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
+import java.util.NoSuchElementException;
 
 public class MoorClassLoader extends ClassLoader {
 	private boolean debugEnabled;
-	private final String[] path;
-	private final ArrayList<URL> urls;
+	private boolean traceEnabled;
+	private final URL[] path;
 	private final boolean parentLast;
 
 	private final HashMap<String, Class<?>> classes;
@@ -26,19 +24,19 @@ public class MoorClassLoader extends ClassLoader {
 		this(getSystemClassLoader());
 	}
 
-	public MoorClassLoader(String path) {
-		this(new String[] { path });
+	public MoorClassLoader(URL path) {
+		this(new URL[] { path });
 	}
 
-	public MoorClassLoader(String[] path) {
+	public MoorClassLoader(URL[] path) {
 		this(false, path);
 	}
 
-	public MoorClassLoader(boolean parentLast, String path) {
-		this(parentLast, new String[] { path });
+	public MoorClassLoader(boolean parentLast, URL path) {
+		this(parentLast, new URL[] { path });
 	}
 
-	public MoorClassLoader(boolean parentLast, String[] path) {
+	public MoorClassLoader(boolean parentLast, URL[] path) {
 		this(getSystemClassLoader(), parentLast, path);
 	}
 
@@ -47,29 +45,34 @@ public class MoorClassLoader extends ClassLoader {
 	}
 
 	public MoorClassLoader(ClassLoader parent, boolean parentLast) {
-		this(parent, parentLast, (String[]) null);
+		this(parent, parentLast, (URL[]) null);
 	}
 
-	public MoorClassLoader(ClassLoader parent, boolean parentLast, String path) {
-		this(parent, parentLast, new String[] { path });
+	public MoorClassLoader(ClassLoader parent, boolean parentLast, URL path) {
+		this(parent, parentLast, new URL[] { path });
 	}
 
-	public MoorClassLoader(ClassLoader parent, boolean parentLast, String[] path) {
+	public MoorClassLoader(ClassLoader parent, boolean parentLast, URL[] path) {
 		super(parent);
 
-		ArrayList<String> systemClassPath = parsePath(System.getProperty(
+		ArrayList<URL> systemClassPath = parsePath(System.getProperty(
 				"java.class.path").split(":"));
-		ArrayList<String> userClassPath = parsePath(path);
+		ArrayList<URL> userClassPath = parsePath(path);
 
-		ArrayList<String> pathList = new ArrayList<String>(
-				systemClassPath.size() + userClassPath.size());
-		pathList.addAll(systemClassPath);
-		pathList.addAll(userClassPath);
+		ArrayList<URL> pathList = new ArrayList<URL>(systemClassPath.size()
+				+ userClassPath.size());
 
-		path = new String[pathList.size()];
+		if (parentLast) {
+			pathList.addAll(userClassPath);
+			pathList.addAll(systemClassPath);
+		} else {
+			pathList.addAll(systemClassPath);
+			pathList.addAll(userClassPath);
+		}
+
+		path = new URL[pathList.size()];
 
 		this.path = pathList.toArray(path);
-		this.urls = getUrls(this.path);
 		this.parentLast = parentLast;
 		this.classes = new HashMap<String, Class<?>>();
 	}
@@ -132,7 +135,8 @@ public class MoorClassLoader extends ClassLoader {
 		InputStream is = null;
 		ByteArrayOutputStream byteStream = null;
 		try {
-			is = getFromPath(className);
+			URL u = getFromPath(this.path, classNameToFile(className));
+			is = u.openStream();
 
 			byteStream = new ByteArrayOutputStream();
 			int nextValue = is.read();
@@ -164,101 +168,171 @@ public class MoorClassLoader extends ClassLoader {
 
 	@Override
 	protected URL findResource(String name) {
-		return super.findResource(name);
+		if (parentLast) {
+			URL u = getFromPath(this.path, name);
+			return u != null ? u : super.findResource(name);
+		} else {
+			URL u = super.findResource(name);
+			return u != null ? u : getFromPath(this.path, name);
+		}
 	}
 
 	@Override
 	protected Enumeration<URL> findResources(String name) throws IOException {
-		return super.findResources(name);
+		return new ResourcesEnumeration(this.path, super.findResources(name),
+				name, parentLast);
 	}
 
-	private InputStream getFromPath(String className) {
-		for (String s : this.path) {
-			InputStream is = null;
-			File f = new File(s);
-			if (f.isDirectory()) {
-				is = getFromDir(className, f);
-			} else if (f.isFile() && f.getAbsolutePath().endsWith(".jar")) {
-				is = getFromJar(className, f);
-			}
-
-			if (is != null)
-				return is;
-		}
-
-		return null;
-	}
-
-	private ArrayList<String> parsePath(String[] cp) {
-		ArrayList<String> list;
+	private ArrayList<URL> parsePath(String[] cp) {
+		ArrayList<URL> list;
 		if (cp != null) {
-			list = new ArrayList<String>(cp.length);
+			list = new ArrayList<URL>(cp.length);
 			for (String s : cp) {
 				if (s == null)
 					continue;
+
 				if (s.isEmpty())
 					continue;
+
 				File f = new File(s);
-				if (f.isDirectory())
-					list.add(f.getAbsolutePath());
-				else if (f.isFile() && f.getAbsolutePath().endsWith(".jar"))
-					list.add(f.getAbsolutePath());
+				try {
+					if (f.isDirectory())
+						list.add(new URL("file", "", f.getAbsolutePath() + "/"));
+					else if (f.isFile() && f.getAbsolutePath().endsWith(".jar"))
+						list.add(new URL("jar", "", "file://"
+								+ f.getAbsolutePath() + "!/"));
+				} catch (MalformedURLException e) {
+					if (traceEnabled)
+						e.printStackTrace();
+				}
 			}
 		} else
-			list = new ArrayList<String>(0);
+			list = new ArrayList<URL>(0);
 		return list;
 	}
 
-	private InputStream getFromDir(String className, File dir) {
-		File target = new File(dir.getAbsolutePath(),
-				classNameToFile(className));
+	private ArrayList<URL> parsePath(URL[] cp) {
+		ArrayList<URL> list;
+		if (cp != null) {
+			list = new ArrayList<URL>(cp.length);
+			for (URL s : cp) {
+				if (s == null)
+					continue;
 
-		try {
-			FileInputStream stream = new FileInputStream(target);
-			if (isDebugEnabled())
-				System.out.println("found class " + className + " in dir: "
-						+ dir.getAbsolutePath());
-			return stream;
-		} catch (Exception e) {
-		}
-		return null;
+				if ("file".equalsIgnoreCase(s.getProtocol())) {
+					File f = new File(s.getFile());
+					try {
+						if (f.isDirectory())
+							list.add(new URL("file", "", f.getAbsolutePath()
+									+ "/"));
+						else if (f.isFile()
+								&& f.getAbsolutePath().endsWith(".jar"))
+							list.add(new URL("jar", "", s.toExternalForm()
+									+ "!/"));
+					} catch (MalformedURLException e) {
+						if (traceEnabled)
+							e.printStackTrace();
+					}
+				} else {
+					list.add(s);
+				}
+			}
+		} else
+			list = new ArrayList<URL>(0);
+		return list;
 	}
 
-	private InputStream getFromJar(String className, File jar) {
-		try {
-			JarFile jarFile = new JarFile(jar);
-			JarEntry entry = jarFile.getJarEntry(classNameToFile(className));
-			if (entry == null)
-				return null;
-			InputStream is = jarFile.getInputStream(entry);
-			if (isDebugEnabled())
-				System.out.println("found class " + className
-						+ " in jar file: " + jar.getAbsolutePath());
-			return is;
-		} catch (Exception e) {
-		}
-		return null;
-	}
+	private URL getFromPath(URL[] urls, String resourceName) {
+		for (URL s : urls) {
+			URL is = null;
+			if ("file".equalsIgnoreCase(s.getProtocol())) {
+				File f = new File(s.getFile());
+				if (f.isDirectory())
+					is = getFromClassDir(s, resourceName);
 
-	private ArrayList<URL> getUrls(String[] path) {
-		ArrayList<URL> urls = new ArrayList<URL>(path.length + 10);
-		for (String s : path) {
-			try {
-				urls.add(new URL("file", "", s));
-			} catch (MalformedURLException e) {
-				e.printStackTrace();
+			} else if ("jar".equalsIgnoreCase(s.getProtocol())) {
+				is = getFromJar(s, resourceName);
+			} else {
+				// FIXME
+				try {
+					is = new URL(s, resourceName);
+				} catch (MalformedURLException e) {
+					if (traceEnabled)
+						e.printStackTrace();
+				}
+			}
+
+			if (is != null) {
+				if (debugEnabled)
+					System.out.println(this.toString() + " found resource "
+							+ resourceName + " in " + s);
+				return is;
 			}
 		}
 
-		return urls;
+		return null;
+	}
+
+	private URL getFromClassDir(URL dir, String resourceName) {
+		File target = new File(dir.getFile(), resourceName);
+
+		if (target.exists())
+			try {
+				return new URL(dir, resourceName);
+			} catch (MalformedURLException e) {
+				if (traceEnabled)
+					e.printStackTrace();
+			}
+
+		return null;
+	}
+
+	private URL getFromJar(URL jar, String resourceName) {
+		try {
+			URL u = new URL(jar, resourceName);
+			InputStream is = null;
+			try {
+				is = u.openStream();
+				if (is != null)
+					return u;
+			} finally {
+				if (is != null)
+					try {
+						is.close();
+					} catch (IOException e) {
+					}
+			}
+		} catch (MalformedURLException e) {
+			if (traceEnabled)
+				e.printStackTrace();
+		} catch (IOException e) {
+		}
+		return null;
+	}
+
+	private URL getFromLibDir(URL dir, String resourceName) {
+		for (File f : new File(dir.getFile()).listFiles()) {
+			if (f.isFile() && f.getAbsolutePath().endsWith(".jar")) {
+				URL jar;
+				try {
+					jar = new URL("jar", "", "file://" + f.getAbsolutePath()
+							+ "!/");
+					URL newJar = getFromJar(jar, resourceName);
+					if (newJar != null)
+						return newJar;
+				} catch (MalformedURLException e) {
+					if (traceEnabled)
+						e.printStackTrace();
+				}
+			}
+
+		}
+
+		return null;
 	}
 
 	private String classNameToFile(String className) {
 		return className.replace('.', '/').concat(".class");
-	}
-
-	private String fileNameToClassName(String fileName) {
-		return fileName.replaceAll("\\.class", "").replace('/', '.');
 	}
 
 	public boolean isDebugEnabled() {
@@ -269,24 +343,110 @@ public class MoorClassLoader extends ClassLoader {
 		this.debugEnabled = debugEnabled;
 	}
 
-	private class MoorResourcesEnumeration implements Enumeration<URL> {
+	private class ResourcesEnumeration implements Enumeration<URL> {
 		private final Iterator<URL> it;
 		private final Enumeration<URL> supers;
+		private final boolean parentLast;
 
-		MoorResourcesEnumeration(Iterator<URL> it, Enumeration<URL> supers) {
-			this.it = it;
+		ResourcesEnumeration(URL[] urls, Enumeration<URL> supers, String name,
+				boolean parentLast) {
 			this.supers = supers;
+			this.parentLast = parentLast;
+			ArrayList<URL> u = getFromPath(urls, name);
+			this.it = u.iterator();
 		}
 
 		@Override
 		public boolean hasMoreElements() {
-			return this.it.hasNext();
+			if (this.parentLast) {
+				return this.it.hasNext() ? this.it.hasNext() : supers
+						.hasMoreElements();
+			} else {
+				return supers.hasMoreElements() ? supers.hasMoreElements()
+						: this.it.hasNext();
+			}
 		}
 
 		@Override
 		public URL nextElement() {
-			return this.it.next();
+			if (this.parentLast) {
+				try {
+					return this.it.next();
+				} catch (NoSuchElementException e) {
+					return supers.nextElement();
+				}
+			} else {
+				try {
+					return supers.nextElement();
+				} catch (NoSuchElementException e) {
+					return this.it.next();
+				}
+			}
 		}
 
+		private ArrayList<URL> getFromPath(URL[] urls, String resourceName) {
+			ArrayList<URL> list = new ArrayList<URL>(urls.length);
+			for (URL s : urls) {
+				URL is = null;
+				if ("file".equalsIgnoreCase(s.getProtocol())) {
+					File f = new File(s.getFile());
+					if (f.isDirectory())
+						is = getFromClassDir(s, resourceName);
+
+				} else if ("jar".equalsIgnoreCase(s.getProtocol())) {
+					is = getFromJar(s, resourceName);
+				} else {
+					// FIXME
+					try {
+						is = new URL(s, resourceName);
+					} catch (MalformedURLException e) {
+						if (traceEnabled)
+							e.printStackTrace();
+					}
+				}
+
+				if (is != null)
+					list.add(is);
+			}
+
+			return list;
+		}
+
+		private URL getFromClassDir(URL dir, String resourceName) {
+			File target = new File(dir.getFile(), resourceName);
+
+			if (target.exists())
+				try {
+					return new URL(dir, resourceName);
+				} catch (MalformedURLException e) {
+					if (traceEnabled)
+						e.printStackTrace();
+				}
+
+			return null;
+		}
+
+		private URL getFromJar(URL jar, String resourceName) {
+			try {
+				URL u = new URL(jar, resourceName);
+				InputStream is = null;
+				try {
+					is = u.openStream();
+					if (is != null)
+						return u;
+				} finally {
+					if (is != null)
+						try {
+							is.close();
+						} catch (IOException e) {
+						}
+				}
+			} catch (MalformedURLException e) {
+				if (traceEnabled)
+					e.printStackTrace();
+			} catch (IOException e) {
+			}
+			return null;
+		}
 	}
 }
